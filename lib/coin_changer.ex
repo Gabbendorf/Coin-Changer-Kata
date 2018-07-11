6d38ef22f4bf5edf8861f6e0a6e2ae2e83a56a4c
Gabba <gmedas@8thlight.com>
2018-07-11 14:58:45 +0100

    Changes variables style conforming to elixir.

--- lib/coin_changer.ex
+++ lib/coin_changer.ex
@@ -3,15 +3,15 @@ defmodule CoinChanger do
   @coins [50, 20, 10, 5, 2, 1]
 
   def change(amount, changes) do
-    if exactChange?(amount) do
+    if exact_change?(amount) do
       update(changes, amount)
     else
-      smallerCoin = Enum.find(@coins, fn(coin) -> coin < amount end)
-      change(amount - smallerCoin, update(changes, smallerCoin))
+      smaller_coin = Enum.find(@coins, fn(coin) -> coin < amount end)
+      change(amount - smaller_coin, update(changes, smaller_coin))
     end
   end
 
-  defp exactChange?(amount) do
+  defp exact_change?(amount) do
     Enum.member?(@coins, amount)
   end
 
