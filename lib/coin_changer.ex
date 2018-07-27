c13720a438414c457b81c6eab65f7ad2adaeb64c
Gabba <gmedas@8thlight.com>
2018-07-27 10:42:36 +0100

    Refactorings.

--- lib/coin_changer.ex
+++ lib/coin_changer.ex
@@ -2,20 +2,10 @@ defmodule CoinChanger do
 
   @coins [50, 20, 10, 5, 2, 1]
 
+  def change(amount, changes) when amount in @coins, do: [amount | changes]
+
   def change(amount, changes) do
-    if exact_change?(amount) do
-      update(changes, amount)
-    else
-      smaller_coin = Enum.find(@coins, fn(coin) -> coin < amount end)
-      change(amount - smaller_coin, update(changes, smaller_coin))
-    end
-  end
-
-  defp exact_change?(amount) do
-    Enum.member?(@coins, amount)
-  end
-
-  defp update(changes, amount) do
-    List.insert_at(changes, 0, amount)
+    smaller_coin = Enum.find(@coins, fn(coin) -> coin < amount end)
+    change(amount - smaller_coin, [smaller_coin | changes])
   end
 end
